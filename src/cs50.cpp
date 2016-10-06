86b78e5d3bbe6c83f74d4be2b394455630ecc687
Andrej Petelin <petelin.andrej@amis.net>
2016-10-06 23:21:17 +0200

    reimplemented get_string, get_char, get_int, get_long_long, compatible with C++98 standard, implemented a centralized read input/input error handling function

--- src/cs50.cpp
+++ src/cs50.cpp
@@ -1,12 +1,49 @@
 #include <climits>
 #include <iostream>
-#include <regex>
+#include <stdexcept>
 #include <string>
+#include <sstream>
 
 #include "cs50.h"
 
 namespace cs50
 {
+
+/**
+ * Reads a line of text from standard input and returns it as a std::string;
+ * The function is used internally for reading input and handling console input
+ * stream errors. If eof or input stream corrupt (failed to read into string).
+ * The argument passed to bool *ok is used to check for eof or bad string input
+ * so it's set to false when read fails or is in eof state, and set to true
+ * otherwise.
+ */
+std::string read_input(bool *ok)
+{
+    std::string str;
+
+    // attempt to read string input into s
+    std::getline(std::cin, str);
+
+    // check if input stream in bad state (hardware failure?)
+    if (std::cin.bad())
+    {
+        // if we're here we can't recover the cin stream
+        // we only get here in case of catastrophic error so throw
+        throw (std::runtime_error("cs50::input_string: error reading input"));
+    }
+
+    if (std::cin.eof() || std::cin.fail())
+    {
+        // report bad string input via side-effect and clear cin flags
+        *ok = false;
+        std::cin.clear();
+        return std::string();
+    }
+
+    // if we're here all is ok so we set sentinel to true and return the input
+    *ok = true;
+    return str;
+}
 
 /**
  * TODO
@@ -26,17 +63,18 @@ char get_char()
 {
     while (true)
     {
-        std::string str = get_string();
+        // sentinel bool, false on EOF or failed reading of input
+        bool valid;
+        std::string str = read_input(&valid);
 
-        // if eof, failbit or badbit clear state and return CHAR_MAX
-        if (std::cin.eof() || std::cin.fail())
+        // on EOF, bad string input
+        if (valid == false)
         {
-            std::cin.clear();
             return CHAR_MAX;
         }
 
-        // return 1st char of str only if single char of input 
-        else if (str.size() == 1)
+        // return 1st char of str only if single char of input
+        if (str.size() == 1)
         {
             return str[0];
         }
@@ -77,29 +115,25 @@ int get_int()
     // attempt to take an int from the user
     while (true)
     {
-        std::string str = get_string();
+        bool valid;
+        std::string str = read_input(&valid);
 
-        // if eof, failbit or badbit clear state and return INT_MAX
-        if (std::cin.eof() || std::cin.fail())
+        // on bad string input (EOF)
+        if (!valid)
         {
-            std::cin.clear();
             return INT_MAX;
         }
 
-        // regex for 0 or 1 + or -, followed by one or more digits
-        std::regex re("[+-]?\\d+");
-
-        // on digits-only input attempt to convert string to int
-        if (std::regex_match(str, re))
+        // check if there's input with no whitespace at beginning
+        if (str.length() > 0 && !isspace(str[0]))
         {
-            try
+            std::istringstream iss(str);
+            int ret;
+
+            // return if iss successfully read into an int and iss exhausted
+            if (iss >> ret && iss.eof())
             {
-                return std::stoi(str);
-            }
-            // in case of value exceeding int size do nothing and reprompt
-            catch (const std::exception&)
-            {
-                ;
+                return ret;
             }
         }
 
@@ -111,76 +145,56 @@ int get_int()
 /**
  * Reads a line of text from standard input and returns the equivalent long
  * long int; if string does not represent a long long int, user is prompted to
- * retry. If eof or input stream corrupt (failed to read into string) or int
- * out of range returns LLONG_MAX.
+ * retry. If eof or input stream corrupt (failed to read into string) or out of
+ * range returns LLONG_MAX.
  */
 long long get_long_long()
 {
+    // attempt to take an int from the user
     while (true)
     {
-        std::string str = get_string();
+        bool valid;
+        std::string str = read_input(&valid);
 
-        // if eof, failbit or badbit clear state and return LLONG_MAX
-        if (std::cin.eof() || std::cin.fail())
+        // on bad string input (EOF)
+        if (!valid)
         {
-            std::cin.clear();
             return LLONG_MAX;
         }
 
-        // regex for 0 or 1 + or -, followed by one or more digits
-        std::regex re("[+-]?\\d+");
-
-        // on digits-only input attempt to convert string to long long int
-        if (std::regex_match(str, re))
+        // check if there's input with no whitespace at the beginning
+        if (str.length() > 0 && !isspace(str[0]))
         {
-            try
+            std::istringstream iss(str);
+            long long ret;
+
+            // return when iss successfully read into long long & iss exhausted
+            if (iss >> ret && iss.eof())
             {
-                return std::stoll(str);
-            }
-            // in case of value exceeding int size do nothing and reprompt
-            catch (const std::exception&)
-            {
-                ;
+                return ret;
             }
         }
 
         // if we're here the input was not ok so reprompt
         std::cout << "Retry: ";
-
     }
 }
 
 /**
- * TODO
+ * reads a line of text from standard input and returns it as std::string. If
+ * input is invalid it returns an empty std::string.
  */
 std::string get_string(void)
 {
-    // TODO: check for failure
     // TODO: decide whether to return string or c_str
-    std::string s;
+    bool valid;
+    std::string s = read_input(&valid);
 
-    // attempt to read string input into s, report error if failed
-    try
+    // return empty string on invalid input
+    if (valid == false)
     {
-        std::getline(std::cin, s);
+        return std::string();
     }
-    catch (const std::exception& e)
-    {
-        // report error and rethrow in case someone's handling it downstream
-        std::cerr << e.what() << std::endl;
-        throw e;
-    }
-
-    // check if input stream in bad state (hardware failure?)
-    if (std::cin.bad())
-    {
-        // NOTE - stream not cleared here for eof/fail checks downstream
-        // if we're here we can't recover the cin stream
-        std::cerr << "cs50 get_string error: cannot read input!\n";
-        // we only get here in case of catastrophic error so throw
-        throw (std::runtime_error("cs50::get_string: error reading input"));
-    }
-
     return s;
 }
 
