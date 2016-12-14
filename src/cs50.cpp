1796a01168f3ccb29e92cb934e1d811026b7ae1d
The Joule <IvanJasenov@users.noreply.github.com>
2016-12-14 03:55:05 +0100

    Proposal
    Here I didn't go with reinventing the wheel, relied to lib50-c as the backbone.
    Whitespace cut-off is carried in get_string method.
    I've had in mind the restriction of C++99 all the time.
    Want to hear if this design passes the bar. If so, GC and some other features which will improve the robustness can be easily added later on. 
    
    -Ivan

--- src/cs50.cpp
+++ src/cs50.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-#include <string>
-
+#include <stdexcept>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h> // for sscanf
+#include <float.h> // for FLT_MIN and FLT_MAX
+#include <math.h> // for isfinite
+// using the namespace std just for development
+using namespace std;
 namespace cs50
 {
 
@@ -17,8 +25,26 @@ void eprintf()
  */
 char get_char(void)
 {
-    // TODO
-    return '\0';
+    while (true)
+    {
+        // get line of text, returning CHAR_MAX on failure
+        const char* local = get_string();
+        static char c; // just a placeholder
+		
+		// return a char if only a char was provided
+        if (strlen(local) > 0 && strlen(local) <= 1 && isalpha(local[0]) && local)
+        {
+           	c = local[0]; 
+        	return c;
+        	
+        }
+        if (strlen(local) > 0 && *local == '\0')
+        {
+        	return CHAR_MAX;
+        	
+        }
+        cout << "Retry: " << endl;
+    }
 }
 
 /**
@@ -26,8 +52,30 @@ char get_char(void)
  */
 double get_double(void)
 {
-    // TODO
-    return 0.0;
+    while (true)
+    {
+        // get line of text, returning DBL_MAX on failure
+        const char* local = get_string();
+        static double d;
+        if (strlen(local) > 0 && *local)
+        {
+            static char* tail;
+            errno = 0;;
+            d = strtod(local, &tail);
+            if (errno == 0 && *tail == '\0' && isfinite(d) != 0 && d < DBL_MAX)
+            {
+                if (strcspn(local, "XxEePp") == strlen(local))
+                {
+                    return d;
+                }
+            }
+        }
+        //if there is something in the string but buffer is full, return the max double value
+        if (strlen(local) > 0 && *local == '\0')
+        {
+         	return DBL_MAX;
+        }
+        printf("Retry: ");
 }
 
 /**
@@ -35,8 +83,26 @@ double get_double(void)
  */
 float get_float(void)
 {
-    // TODO
-    return 0.0;
+    while (true)
+	{
+		const char* local = get_string();
+		static float flt; // a placeholder
+		
+		if (strlen(local) > 0 && atof(local) && local)
+		{
+			flt = atof(local);
+			if (flt >= FLT_MIN || flt < FLT_MAX) // check for being in valid range
+			{
+				return flt;
+            }
+			else
+			{
+				return FLT_MAX;
+            }
+			// dissalow hex input format as done in C lib...TODO
+		}
+		cout << "Retry: " << endl;
+	}
 }
 
 /**
@@ -45,7 +111,28 @@ float get_float(void)
 int get_int(void)
 {
     // TODO
-    return 0;
+    while (true)
+	{
+		const char* local = get_string();
+		static long tmp; // long, since stoi in not c++99 compatib
+		
+		if (strlen(local) > 0 && local)
+		{
+			static char* tail = NULL;              // number base is 10
+			tmp = strtol(local, &tail, 10); // tail takes care of iterating to the NULL, strtol has a looping construct
+			if (errno == 0 && *tail == '\0' && tmp >= INT_MIN && tmp < INT_MAX)
+			{
+				return tmp;
+			}
+
+		}
+		if (strlen(local) > 0 && local == '\0')
+		{
+			return INT_MAX;
+		}
+		// this will be prompt out in case if nothing is entered, just hit ENTER 
+		std::cout << "Retry: " << std::endl;
+	}
 }
 
 /**
@@ -53,20 +140,64 @@ int get_int(void)
  */
 long long get_long_long(void)
 {
-    // TODO
-    return 0;
+    while (true)
+    {
+        // get line of text, returning LLONG_MAX on failure
+        const char* local = get_string();
+        static long long n;
+        
+        if (strlen(local) && local)
+        {
+            char* tail;
+            errno = 0;
+            n = strtoll(local, &tail, 10);
+            if (errno == 0 && *tail == '\0' && n < LLONG_MAX)
+            {
+                return n;
+            }
+        }
+        printf("Retry: ");
+    }
 }
 
 /**
  * TODO
  */
+ // acts like a parser for the other methods yet to follow
+// discarding whitespace characters is done in get_string
 std::string get_string(void)
 {
     // TODO: check for failure
     // TODO: decide whether to return string or c_str
     std::string s;
     std::getline(std::cin, s);
-    return s;
+    std::string s;
+	getline(std::cin, s);
+	// check for failure on string class 
+	if(!std::cin.good()) // if not good
+	{
+		throw std::invalid_argument("Something went wrong");
+	}
+	else if(std::cin.eof()) // if not enough memory
+	{
+		std::cout << "EOF" << std::endl;
+		throw std::invalid_argument("EOF");
+	}
+	// do the conversion c_str
+	// I have a problem when input is isneted with spaces...do something to cut off the whitespaces
+	static int len = s.length();
+	char* str_tmp = new char(len + 1);
+	strcpy(str_tmp, s.c_str());
+	// make a cut off
+	static char* str; 
+	while (isspace(*str_tmp) && *str_tmp != '\0') // while not space and *str_tmp != NULL
+	{
+		(void)*str_tmp++;
+	}
+	// now share a pointee
+	str = str_tmp; // you should clean the str 
+	// return without the leading whitespace 
+	return str;
 }
 
 }
